93f3a5296c4ce09b735f61000df48d6e7f3def9e
Ivan Filippov <kaldblrikfinn@gmail.com>
2023-09-18 17:30:55 +0300

    max calories per day

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, StyleSheet } from 'react-native'
-import { Appbar, useTheme } from 'react-native-paper'
+import { Appbar, TextInput, useTheme } from 'react-native-paper'
 
 export const ProfileScreen = () => {
   const theme = useTheme()
+  const [maxCaloriesPerDay, setMaxCaloriesPerDay] = useState(0)
+
+  const onChangeMaxCaloriesPerDay = (calories: string) => {
+    if (isNaN(Number(calories))) return
+    setMaxCaloriesPerDay(Number(calories))
+  }
+  
   return (
-    <View style={style.wrapper}>
+    <View style={styles.wrapper}>
       <Appbar.Header theme={theme} mode="small">
         <Appbar.Content title="Profile" />
       </Appbar.Header>
+      <View style={styles.form}>
+        <TextInput
+          label="Max calories per day"
+          mode="outlined"
+          enterKeyHint='done'
+          keyboardType='number-pad'
+          value={isNaN(maxCaloriesPerDay) ? '0' : maxCaloriesPerDay.toString()}
+          onChangeText={onChangeMaxCaloriesPerDay}
+        />
+      </View>
     </View>
   )
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
   },
+  form: {
+    margin: 16,
+  }
 })
