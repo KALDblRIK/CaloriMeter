033c9225848f2c267b1c3448f2cd777e2a883cab
Ivan Filippov <kaldblrikfinn@gmail.com>
2023-09-29 14:29:25 +0300

    android new icon

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
-import { Appbar, useTheme } from 'react-native-paper'
+import { Appbar, FAB, useTheme } from 'react-native-paper'
 
 export const HomeScreen = () => {
   const theme = useTheme()
@@ -10,6 +10,11 @@ export const HomeScreen = () => {
       <Appbar.Header theme={theme} mode="small">
         <Appbar.Content title="Home" />
       </Appbar.Header>
+      <FAB
+        icon="plus"
+        style={styles.fab}
+        label='Add meal'
+      />
     </View>
   )
 }
@@ -17,5 +22,11 @@ export const HomeScreen = () => {
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
   },
 })
