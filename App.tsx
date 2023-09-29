565a6b7b9a173b94a9c0d032c8432350714b41fa
Ivan Filippov <kaldblrikfinn@gmail.com>
2023-09-29 03:14:27 +0300

    realm

--- App.tsx
+++ App.tsx
@@ -1,22 +1,46 @@
 import React from 'react'
 import { useColorScheme } from 'react-native'
+import Realm from 'realm'
+import { RealmProvider } from '@realm/react'
+
 import { MD3LightTheme, MD3DarkTheme, PaperProvider } from 'react-native-paper'
 import { NavigationContainer } from '@react-navigation/native'
 import { useMaterial3Theme } from '@pchmn/expo-material3-theme'
 
 import { NavigationWrapper } from './src/navigation'
 
-function App(): JSX.Element {
+export class Profile extends Realm.Object<Profile> {
+  _id!: number
+  name!: string
+  maxCaloriesPerDay?: number
+  static schema = {
+    name: 'Profile',
+    properties: {
+      _id: 'int',
+      name: 'string',
+      maxCaloriesPerDay: 'int',
+    },
+    primaryKey: '_id',
+  }
+}
+
+const realmConfig: Realm.Configuration = {
+  schema: [Profile],
+}
+
+const App = () => {
   const colorScheme = useColorScheme()
   const { theme } = useMaterial3Theme({ fallbackSourceColor: '#3DDC84' })
   const paperTheme = colorScheme === 'dark' ? { ...MD3DarkTheme, colors: theme.dark } : { ...MD3LightTheme, colors: theme.light }
 
   return (
-    <PaperProvider theme={paperTheme}>
-      <NavigationContainer>
-        <NavigationWrapper />
-      </NavigationContainer>
-    </PaperProvider>
+    <RealmProvider {...realmConfig}>
+      <PaperProvider theme={paperTheme}>
+        <NavigationContainer>
+          <NavigationWrapper />
+        </NavigationContainer>
+      </PaperProvider>
+    </RealmProvider>
   )
 }
 
--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react'
+import { useObject, useRealm } from '@realm/react'
+import React, { useCallback, useState } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { Appbar, TextInput, useTheme } from 'react-native-paper'
+import { Profile } from '../../../App'
+
 
 export const ProfileScreen = () => {
-  const theme = useTheme()
-  const [maxCaloriesPerDay, setMaxCaloriesPerDay] = useState(0)
+  const realm = useRealm()
+  const profile = useObject(Profile, 0)
 
-  const onChangeMaxCaloriesPerDay = (calories: string) => {
-    if (isNaN(Number(calories))) return
-    setMaxCaloriesPerDay(Number(calories))
-  }
+  const theme = useTheme()
+  const [maxCaloriesPerDay, setMaxCaloriesPerDay] = useState(profile?.maxCaloriesPerDay ?? 0)
+
+  const onChangeMaxCaloriesPerDay = useCallback((value: string) => {
+    const calories = Number(value)
+    if (isNaN(calories)) return
+    setMaxCaloriesPerDay(calories)
+
+    if (profile) {
+      realm.write(() => {
+        profile.maxCaloriesPerDay = calories
+      })
+      
+    } else {
+      realm.write(() => {
+        return realm.create('Profile', { _id: 0, name: 'default', maxCaloriesPerDay: calories })
+      })
+    }
+  }, [profile, realm])
   
   return (
     <View style={styles.wrapper}>
