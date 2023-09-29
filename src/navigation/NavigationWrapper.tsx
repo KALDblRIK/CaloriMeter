e30bb275c45335021349bcd7bf711511f980a7ba
Ivan Filippov <kaldblrikfinn@gmail.com>
2023-09-29 04:10:54 +0300

    navigationBar

--- src/navigation/NavigationWrapper.tsx
+++ src/navigation/NavigationWrapper.tsx
@@ -1,66 +1,19 @@
 import React from 'react'
-import { CommonActions } from '@react-navigation/native'
-import { BottomNavigation } from 'react-native-paper'
-import { BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import { HomeScreen } from '../screens/Home'
 import { HistoryScreen } from '../screens/History'
 import { ProfileScreen } from '../screens/Profile'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import { NavigationBar } from '../components'
 
 const Tab = createBottomTabNavigator()
-
-const TabBar = (props: BottomTabBarProps) => {
-  const { navigation, state, descriptors, insets } = props
-
-  return (
-    <BottomNavigation.Bar
-      navigationState={state}
-      safeAreaInsets={insets}
-      onTabPress={({ route, preventDefault }) => {
-        const event = navigation.emit({
-          type: 'tabPress',
-          target: route.key,
-          canPreventDefault: true,
-        })
-
-        if (event.defaultPrevented) {
-          preventDefault()
-        } else {
-          navigation.dispatch({
-            ...CommonActions.navigate(route.name, route.params),
-            target: state.key,
-          })
-        }
-      }}
-      renderIcon={({ route, focused, color }) => {
-        const { options } = descriptors[route.key]
-        if (options.tabBarIcon) {
-          return options.tabBarIcon({ focused, color, size: 24 })
-        }
-
-        return null
-      }}
-      getLabelText={({ route }) => {
-        const { options } = descriptors[route.key]
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route?.title
-
-        return label
-      }}
-    />
-  )
-}
 
 export const NavigationWrapper = () => {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      tabBar={TabBar}
+      tabBar={NavigationBar}
     >
       <Tab.Screen
         name="Home"
